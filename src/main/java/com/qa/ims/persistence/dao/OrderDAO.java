ae77d235b3fa7b7d5eb6e96905f098405f34c99d
Conor Kelly <conorkelly10@aol.com>
2022-02-18 14:52:29 +0000

    display cost of order when reading orders

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -32,7 +32,7 @@ public static final Logger LOGGER = LogManager.getLogger();
 	public List<Order> readAll() {
 		try (Connection connection = DBUtils.getInstance().getConnection();
 				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");) {
+				ResultSet resultSet = statement.executeQuery("SELECT order_id, id, 	items.item_id, qty, (qty * items.price) AS cost FROM orders, items WHERE orders.item_id = items.item_id");) {
 			List<Order> orders = new ArrayList<>();
 			while (resultSet.next()) {
 				orders.add(modelFromResultSet(resultSet));
@@ -122,5 +122,32 @@ public static final Logger LOGGER = LogManager.getLogger();
 		}
 		return 0;
 	}
+	// creating an add item functionality to order
+//	public Order addItem(Order order) {
+//		try (Connection connection = DBUtils.getInstance().getConnection();
+//				PreparedStatement statement = connection
+//						.prepareStatement("INSERT INTO orders_items (item_id, order_id) VALUES (1, ?)");) {
+//			statement.setLong(1, order.getOrderId());
+//			statement.executeUpdate();
+//			
+//		} catch (Exception e) {
+//			LOGGER.debug(e);
+//			LOGGER.error(e.getMessage());
+//		}
+//		try (Connection connection = DBUtils.getInstance().getConnection();
+//				PreparedStatement statement = connection
+//						.prepareStatement("UPDATE orders_items\r\n"
+//								+ "SET item_id = (SELECT item_id FROM items where item_id LIKE ?) WHERE order_id = ?"
+//								+ "ORDER BY order_item_id DESC LIMIT 1;")) {
+//			statement.setLong(1, order.getItemId());
+//			statement.setLong(2, order.getOrderId());
+//			statement.executeUpdate();
+//			return read(order.getOrderId());
+//		} catch (Exception e) {
+//			LOGGER.debug(e);
+//			LOGGER.error(e.getMessage());
+//		}
+//		return null;
+//	}
 
 }
