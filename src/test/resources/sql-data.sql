6f299cf1674927e22b13b9814e39f2804585e6cc
Conor Kelly <conorkelly10@aol.com>
2022-02-19 14:04:01 +0000

    updated sql data and schema files and created tests fot itemDAO that do not pass

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,3 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `items` (`name`, `price`) VALUES (`Football`, 20.99);
+INSERT INTO `orders` (`id`, `item_id`, `qty`, `cost`) VALUES (1, 1, 1, 0.00);
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,4 +1,7 @@
 DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `items`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `orders_items`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -6,3 +9,35 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `items` (
+	`item_id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) DEFAULT NULL,
+    `price` DOUBLE DEFAULT NULL,
+    PRIMARY KEY (`item_id`) );
+    
+CREATE TABLE IF NOT EXISTS `orders` (
+    `order_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `qty` INT(4) NOT NULL,
+    `cost` DOUBLE(6,2) DEFAULT NULL,
+    `id` int(11),
+    `item_id` int(11),
+    PRIMARY KEY (`order_id`),
+    FOREIGN KEY (`id`) REFERENCES `customers`(`id`),
+    FOREIGN KEY (`item_id`) REFERENCES `items`(`item_id`) );
+    
+CREATE  TABLE IF NOT EXISTS `orders_items` (
+	`order_item_id` int(11) NOT NULL AUTO_INCREMENT,
+	`item_id` int(11) NOT NULL,
+    `order_id` int(11) NOT NULL,
+    PRIMARY KEY (`order_item_id`),
+    FOREIGN KEY (`item_id`) REFERENCES `items`(`item_id`),
+    FOREIGN KEY (`order_id`) REFERENCES `orders`(`order_id`));
+    
+    SELECT o.order_id, c.id, i.item_id
+FROM (((orders o JOIN customers c ON o.id = c.id)
+JOIN orders_items oi ON o.order_id = oi.order_id)
+JOIN items i ON oi.item_id = i.item_id)
+GROUP BY o.order_id
+ORDER BY o.order_id;
+
